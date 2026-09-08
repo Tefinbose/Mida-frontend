09e081271cd871e592f454d01f90a0d2eef7e3fb
Tefin bose <tefinbose2003@gmail.com>
2026-09-08 16:23:02 +0530

    deplyment issue

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -8,7 +8,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 // Logo
-import MidaLogo from "../../assets/MidaLogo.png";
+import MidaLogo from "../../assets/MidaLogo.png"; 
 
 const navLinks = [
   { name: "Home", path: "/" },
